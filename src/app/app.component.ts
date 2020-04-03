c4cf26837d5d20a0bbbf060029cf202620c0ce69
ThonyBG <amosquera@bgeneral.com>
2020-04-02 19:28:42 -0500

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-downloads';
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  cancel() {
+    this.destroy$.next(true);
+  }
+
+  send() {
+    this.httpClient.get('http://localhost:8888/fakedownload').pipe(takeUntil(this.destroy$)).subscribe(() => {
+      console.log('descargado: ');
+    }, (error) => {
+      console.log('cancelled: ', error);
+    });
+  }
 }
